f3fcad2757be02d284943ddbe8d269ec6a39118c
szhang99 <szhang99@terpmail.umd.edu>
2024-12-01 20:27:43 -0500

    handshake & bitfield

--- tests/test_peer.py
+++ tests/test_peer.py
@@ -1,13 +1,22 @@
+# to run this i ran python tests/test_peer.py in final-project folder
+# i wanted to run it separate from all the other test suite
 # importing sys
 import sys
 
 sys.path.insert(0, '.')
 from torrentula.core.peer import Peer
+from torrentula.utils.helpers import configure_logging
 import socket
 
-peer = Peer("72.235.13.154", 6881, socket.socket(socket.AF_INET, socket.SOCK_STREAM))
+configure_logging()
+peer = Peer("72.235.13.154", 6881, b'\xf3\x1bC\xc3\xa4\x1e\xd3\xf3\xf3\xa4\xd7\x83\x1e~\x9d^\x94ED9', b'12345678901234567890', socket.socket(socket.AF_INET, socket.SOCK_STREAM))
 peer.connect()
-peer.handshake(b'\xf3\x1bC\xc3\xa4\x1e\xd3\xf3\xf3\xa4\xd7\x83\x1e~\x9d^\x94ED9', b'12345678901234567890')
+peer.handshake()
+peer.receive_messages()
+print(peer.received_handshake)
+peer.receive_messages()
+print(peer.received_handshake)
+
 
 # look in wireshark to see if packet is as intended
 i = 0
--- torrentula/core/peer.py
+++ torrentula/core/peer.py
@@ -16,13 +16,22 @@ class MessageType(Enum):
     PIECE = 7
     CANCEL = 8
 
+class Handshake(Enum):
+    # didn't receive handshake
+    HANDSHAKE_NOT_RECVD = 0
+    # means received handshake and can still receive bitmap
+    CAN_RECV_BITFIELD = 1
+    # means received handshake and can't receive bitmap
+    HANDSHAKE_RECVD = 2
+
+
 
 class Peer:
     """
     Represents a peer Bittorrent client in the same swarm.
     """
 
-    def __init__(self, ip_address, port, socket=None, v4=True):
+    def __init__(self, ip_address, port, info_hash, peer_id, socket=None, v4=True):
         """
         Socket argument is provided when the client accepted this connection from a peer rather than initiaiting it.
         """
@@ -51,6 +60,12 @@ class Peer:
         self.last_received = None # Time of last message received from peer
         self.last_sent = None # Time of last message sent to peer
 
+        self.received_handshake = Handshake.HANDSHAKE_NOT_RECVD # check if we recieved a handshake or not
+        self.info_hash = info_hash # we keep an info hash here to send and check when received
+        self.peer_id = peer_id # we keep a peer_id here in the case of them receiving 
+        self.sent_handshake = False # needed to differentiate if we initiate or they initiate connection
+        self.can_send_bitfield = False # client checks and handles sending bitfields
+
     def connect(self):
         """ 
         Should throw an error on fail.
@@ -65,20 +80,17 @@ class Peer:
         self.socket.close()
         self.socket = None
 
-    def handshake(self, info_hash, peer_id):
+    def handshake(self):
         """
         Send handshake message.
         """
         pstr = "BitTorrent protocol"
         pstrlen = len(pstr)
-        info_hash = info_hash
-        peer_id = peer_id
         # ! = big endian, B = unsigned char, then string s, then 8 padding 0s, then 2 20 length strings
-        msg = struct.pack(f"!B{pstrlen}s8x20s20s", pstrlen, pstr.encode('utf-8'), info_hash, peer_id)
-        logger.debug(msg)
-        self.socket.sendall(msg)
-        
-        
+        # I don't expect to use these 8 padding bytes
+        msg = struct.pack(f"!B{pstrlen}s8x20s20s", pstrlen, pstr.encode('utf-8'), self.info_hash, self.peer_id)
+        self.send_msg(msg)
+        self.sent_handshake = True
 
     def upload(self, block):
         pass
@@ -90,10 +102,42 @@ class Peer:
         pass
 
     def receive_messages(self):
-        pass
+        """
+        Assuming we do the select outside of receive_messages - just handle messages here
+        if we haven't received a handshake we expect a handshake before anything else
+        If we receive something wrong we disconnect
+        Returns -1 on error, 1 on correct handling
 
-    def poll(self):
-        pass
+        This code does not really handle blocking problems on recv, could cause issues in the future
+        we also don't really handle unexpected recvs, could cause some erroring that may need to be caught
+        """
+        if self.received_handshake == Handshake.HANDSHAKE_NOT_RECVD:
+            pstrlen = int.from_bytes(self.socket.recv(1))
+            bytes = self.socket.recv(pstrlen + 48)
+            pstr, padding, info_hash, peer_id = struct.unpack(f"!{pstrlen}s8s20s20s", bytes)
+            logger.debug(f"received {pstr}, {padding}, {info_hash}, {peer_id}")
+            # may want to do smth with pstr, padding, and peer_id 
+            # but rn I only care about checking info_hash
+            if info_hash != self.info_hash:
+                self.disconnect()
+                return -1
+            self.received_handshake = Handshake.CAN_RECV_BITFIELD
+            # if the connection was an incoming connection we still need to send our side of the handshake
+            if not self.sent_handshake:
+                self.handshake()
+            # when we return we expect client object to check this value and send bitfield if needed
+            self.can_send_bitfield = True
+        else:
+            # once we receive any other message other than a handshake we can't receive a bitmap anymore
+            self.received_handshake = Handshake.HANDSHAKE_RECVD
+            msg_len = int.from_bytes(self.socket.recv(4), "big")
+            logger.debug(f"msg len: {msg_len}")
+            # differentiating from keepalive, we don't have to do anything if it's a keepalive
+            if (msg_len > 0):
+                msg_type = int.from_bytes(self.socket.recv(1), "big")
+                logger.debug(f"msg type: {msg_type}")
+        self.last_received = datetime.now()
+        return 1
 
     def send_interested(self):
         pass
@@ -104,8 +148,25 @@ class Peer:
     def send_event(self, type):
         pass
 
-    def send_msg(self):
-        pass
+    def send_bitfield(self, bitfield):
+        """
+        Send the bitfield that we have
+        """
+        bitfield_length = len(bitfield)
+        msg_length = len(bitfield) + 1 
+        msg = struct.pack(f"!iB{bitfield_length}s", msg_length, MessageType.BITFIELD.value, bitfield)
+        self.send_msg(msg)
+
+    def send_msg(self, msg):
+        """
+        Send msg and update last sent time
+        """
+        # once we send any other message we can't send a bitfield anymore
+        self.can_send_bitfield = False
+        logger.debug("sending:")
+        logger.debug(msg)
+        self.last_sent = datetime.now()
+        self.socket.sendall(msg)
 
     def choke(self):
         """
