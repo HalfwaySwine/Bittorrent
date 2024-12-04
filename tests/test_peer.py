c53444f82c8438c24f710ac4d5a9dd600bb4483c
szhang99 <szhang99@terpmail.umd.edu>
2024-12-03 20:55:31 -0500

    peer completed, dubiously

--- tests/test_peer.py
+++ tests/test_peer.py
@@ -10,7 +10,7 @@ from torrentula.utils.helpers import configure_logging
 import hashlib
 import socket
 import select
-
+import time
 
 PIECE_LENGTH = 262144
 BLOCK_SIZE = 16 * 1024    # 16 KB block size
@@ -24,16 +24,24 @@ piece = Piece(0, PIECE_LENGTH, HASH, PIECE_LENGTH, ADD_PATH)
 
 configure_logging()
 # apparently this guy is a seeder that's always running or smth
-peer = Peer("72.235.13.154", 6881, b'\xf3\x1bC\xc3\xa4\x1e\xd3\xf3\xf3\xa4\xd7\x83\x1e~\x9d^\x94ED9', b'12345678901234567890', 2516, socket.socket(socket.AF_INET, socket.SOCK_STREAM))
+peer = Peer("72.235.13.154", 6881, b'\xf3\x1bC\xc3\xa4\x1e\xd3\xf3\xf3\xa4\xd7\x83\x1e~\x9d^\x94ED9', '12345678901234567890', 2516)
 peer.target_piece = piece
 peer.connect()
-peer.handshake()
+
+while True:
+    _, rdy, _ = select.select([], [peer.socket], [], 0)
+    if rdy:
+        peer.is_connected = True
+        break
+peer.send_handshake()
 print("can send bitfield: ", peer.can_send_bitfield)
+_, _, _ = select.select([peer.socket], [], [], 5)
 peer.receive_messages()
 print(peer.received_handshake)
 print("can send bitfield: ", peer.can_send_bitfield)
 # send a bitfield of all 0s
 peer.send_bitfield(bytes(315))
+_, _, _ = select.select([peer.socket], [], [], 1)
 peer.receive_messages()
 print(peer.received_handshake)
 print("can send bitfield: ", peer.can_send_bitfield)
@@ -45,9 +53,8 @@ while True:
     rdy, _, _ = select.select([peer.socket], [], [], 0)
     
     if rdy:
-        print(rdy)
         peer.receive_messages()
-    if peer.peer_choking == False and i < 1:
+    if peer.peer_choking == False and i < 4:
         offset, length = piece.get_next_request()
         peer.send_request(piece, offset, length)
         i += 1
