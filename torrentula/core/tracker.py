7e8a5c16847eec63e06095e2ed5b2ed67500cbd8
Eileen Yuan <eileeny@umd.edu>
2024-12-11 21:17:48 -0500

    added retransmission for udp

--- torrentula/core/tracker.py
+++ torrentula/core/tracker.py
@@ -11,6 +11,9 @@ from datetime import datetime, timedelta
 import random
 import ssl
 
+MAX_RETRIES = 8
+
+
 class Tracker:
     """
     Server managing swarm associated with a torrent. A client can join the swarm by sending a request to this server using its associated url or IP address.
@@ -54,8 +57,9 @@ class Tracker:
         tracker_port = parsed_url.port
         path = parsed_url.path
         if tracker_port == None:
-            tracker_port = 443 if self.type == "HTTPS" else 6969
+            tracker_port = 443 if self.type == "https" else 6969
         tracker_addr = (host, tracker_port)
+
         if self.type == "udp":
             #construct message
             transaction_id = random.randint(0, 2**32 - 1)
@@ -66,20 +70,35 @@ class Tracker:
             announce_request = pack(">QII20s20sQQQIIIiH",
                 self.connection_id, action, transaction_id, self.info_hash, self.peer_id.encode(),
                 downloaded, left, uploaded, event_udp, ip_address, key, TRACKER_NUMWANT, port)
-            self.sock.sendto(announce_request, (host, tracker_port))
-            #recv tracker response
-            response, _ = self.sock.recvfrom(2048)
-            action, received_transaction_id, interval, leechers, seeders = unpack(">IIIII", response[:20])
-            logger.debug(f"UDP tracker response: {action}, {received_transaction_id}, interval: {interval}, leechers: {leechers}, seeders: {seeders}")
-            self.interval = interval
-            peers_data = response[20:]
-            peer_list = []
-            for i in range(0, len(peers_data), 6):
-                ip_tuple = unpack(">BBBB", peers_data[i:i+4])
-                ip_string = '.'.join(map(str, ip_tuple))
-                port = unpack(">H", peers_data[i+4:i+6])[0]
-                peer_list.append(Peer(ip_string, port, self.info_hash, self.peer_id, self.num_pieces))
-            return peer_list
+            initial_timeout = 15
+            for attempt in range(MAX_RETRIES + 1):
+                self.sock.sendto(announce_request, (host, tracker_port))
+                timeout = initial_timeout * (2 ** attempt)
+                self.sock.settimeout(timeout)
+                #recv udp tracker response
+                try:
+                    response, _ = self.sock.recvfrom(2048)
+                    action, received_transaction_id, interval, leechers, seeders = unpack(">IIIII", response[:20])
+                    logger.debug(f"UDP tracker response: {action}, {received_transaction_id}, interval: {interval}, leechers: {leechers}, seeders: {seeders}")
+                    if len(response) < 20 or transaction_id != received_transaction_id or action != 1:
+                        logger.critical(f"UDP tracker response went wrong")
+                        exit(0)
+                    self.interval = interval
+                    peers_data = response[20:]
+                    peer_list = []
+                    for i in range(0, len(peers_data), 6):
+                        ip_tuple = unpack(">BBBB", peers_data[i:i+4])
+                        ip_string = '.'.join(map(str, ip_tuple))
+                        port = unpack(">H", peers_data[i+4:i+6])[0]
+                        peer_list.append(Peer(ip_string, port, self.info_hash, self.peer_id, self.num_pieces))
+                    return peer_list
+                except socket.timeout:
+                    attempt += 1
+                    logger.debug(f"Timeout occurred. Retrying... (Attempt {attempt + 1} of {MAX_RETRIES + 1})")
+                    if attempt == MAX_RETRIES:
+                        logger.critical("Max retries reached. Giving up.")
+                        # Handle the failure case here
+                        exit(0)
         #handle http or https trackers
         params = {
             "peer_id": self.peer_id,
@@ -90,10 +109,10 @@ class Tracker:
             "numwant": TRACKER_NUMWANT,
             "event": event,
         }
-        if self.type == "HTTPS":
+        if self.type == "https":
             context = ssl.create_default_context()
             self.sock = context.wrap_socket(self.sock, server_hostname=host)
-        logger.debug(f"Attempting to connect to tracker at {host}:{tracker_port} over TCP...")
+        logger.debug(f"{self.type}Attempting to connect to tracker at {host}:{tracker_port} over TCP...")
         self.sock.connect(tracker_addr)
         # Construct get request
         encoded_params = urlencode(params)
@@ -206,4 +225,5 @@ class Tracker:
         response, _ = self.sock.recvfrom(1024)
         action, received_transaction_id, connection_id = unpack(">IIQ", response)
         assert(transaction_id == received_transaction_id)
+        assert(action == 0)
         self.connection_id = connection_id
